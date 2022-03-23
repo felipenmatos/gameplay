11b8735bb44133c0d28b0190ddf51862ef18e84d
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-03-23 00:04:00 -0300

    chore: font installation and configuration

--- App.tsx
+++ App.tsx
@@ -1,5 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { useFonts } from 'expo-font';
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import AppLoading from 'expo-app-loading';
+
 import { SingIn } from './src/screens/SingIn';
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  });
+
+  if(!fontsLoaded){
+    <AppLoading /> 
+  }
 
 export default function App() {
   return (
