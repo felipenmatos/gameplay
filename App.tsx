15b9fce0a2f5181d56bdca9ee4793016e1535db5
Felipe Carvalho Nonato Matos <felipe.matos@XDNTRAVEL.CORP>
2022-03-24 19:48:47 -0300

    style: implementation of linear gradient background on pages

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhan
 import AppLoading from 'expo-app-loading';
 
 import { SingIn } from './src/screens/SingIn';
+import { Background } from './src/components/Background';
 
 export default function App() {
 
@@ -21,13 +22,13 @@ export default function App() {
   }
 
   return (
-    <>
+    <Background>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       <SingIn />
-    </>
+    </Background>
   );
 }
